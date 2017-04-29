22a28c62a5e7756459a11b1c93252f77b9e04f2e
Roep, Tamara <troep@ebay.com>
2017-04-29 21:24:01 +0200

    Edited cradle dependencies. Added LeakCanary.

--- app/src/main/java/com/example/troep/cleanexample/App.java
+++ app/src/main/java/com/example/troep/cleanexample/App.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.example.troep.cleanexample.components.DaggerInjector;
 import com.example.troep.cleanexample.components.Injector;
+import com.squareup.leakcanary.LeakCanary;
 
 import timber.log.Timber;
 
@@ -19,6 +20,12 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
         sInstance = this;
         mInjector = DaggerInjector.create();
